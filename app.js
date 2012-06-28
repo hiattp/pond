8e331933f85c0ed21e4ff04abfc721c25b6219e0
Paul Hiatt <hiattp@gmail.com>
2012-06-27 22:22:15 -0400

    began app prep for facebook integration

--- app.js
+++ app.js
@@ -4,15 +4,30 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , async = require('async')
+  , url = require('url')
+  , RedisStore = require('connect-redis')(express);
+  
+// redis session
+var rtg, redis;
+if(process.env.REDISTOGO_URL) {
+  rtg = url.parse(process.env.REDISTOGO_URL);
+  redis = require('redis').createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(':')[1]);
+} else {
+  redis = require('redis').createClient();
+}
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
+  app.use(stylus.middleware({ src: __dirname+'/public' }));
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
