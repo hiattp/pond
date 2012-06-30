cb9d7b89ca55e52996d93c0b947bed347dfa02b6
Paul Hiatt <hiattp@gmail.com>
2012-06-30 16:12:33 -0400

    testing faceplate auth

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,17 @@
 module.exports = function(app){
   
   app.get('/', function(req,res){
+    // console.dir(req.facebook.plate);
     req.facebook.me(function(user){
       res.render('index', {
         user: user
       });
     });
+  });
+  
+  app.post('/', function(req.res){
+    console.log(req.body);
+    res.redirect('/');
   });
   
 }
