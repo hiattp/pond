fa42d1947be7c1b806e678f284a05017f0ecedf0
Paul Hiatt <hiattp@gmail.com>
2012-06-30 14:27:13 -0400

    socket.io functionality working on localhost

--- app.js
+++ app.js
@@ -59,22 +59,13 @@ app.configure('production', function(){
 // Helpers
 
 app.dynamicHelpers({
-  host: function(req,res){
-    return req.headers['host'];
-  },
-  scheme: function(req,res){
-    req.headers['x-forwarded-proto'] || 'http'
-  },
   url: function(req,res){
     return function(path){
-      return app.dynamicViewHelpers.scheme(req, res) + app.dynamicViewHelpers.url_no_scheme(path);
+      var host = req.headers['host'];
+      var scheme = req.headers['x-forwarded-proto'] || 'http';
+      return scheme + "://" + host + path;
     }
   },
-  url_no_scheme: function(req,res){
-    return function(path) {
-      return '://' + app.dynamicViewHelpers.host(req, res) + path;
-    }
-  }
   appID: function(req,res){
     return process.env.FACEBOOK_APP_ID;
   }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   
-  io.connect(socketURL); // socketURL definition in layout.ejs
+  var socket = io.connect(socketURL); // socketURL definition in layout.ejs
   
   socket.on("connections update", function(data){
     $("#connected-users").text(data.total);
