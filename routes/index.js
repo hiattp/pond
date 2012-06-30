6bc830a4843e82a67c897ff1809666e65f07cffa
Paul Hiatt <hiattp@gmail.com>
2012-06-30 16:31:37 -0400

    testing facebook authentication

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@
 module.exports = function(app){
   
   app.get('/', function(req,res){
-    // console.dir(req.facebook.plate);
     req.facebook.me(function(user){
       res.render('index', {
         user: user
@@ -15,8 +14,10 @@ module.exports = function(app){
   });
   
   app.post('/', function(req,res){
-    console.log(req.body);
-    res.redirect('/');
+    res.facebook.plate.parse_signed_request(req.body.signed_request, function(result){
+      console.dir(result);
+      res.redirect('/');
+    });
   });
   
 }
